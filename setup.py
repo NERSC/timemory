8117b7bc8bd6f44e5d72819e16fdae917f1a4ed3
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-02-06 10:36:24 -0800

    PyPi 1.1.8rc0

--- setup.py
+++ setup.py
@@ -363,7 +363,7 @@ def get_email():
 # ---------------------------------------------------------------------------- #
 # calls the setup and declare package
 setup(name='TiMemory',
-    version='1.1.8dev0',
+    version='1.1.8rc0',
     author=get_name(),
     author_email=get_email(),
     maintainer=get_name(),
