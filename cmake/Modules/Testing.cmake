420e7f0e483e0768503caa4eb08b788bfaf97234
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-02-08 18:42:01 -0800

    CDash info improvement for detached

--- cmake/Modules/Testing.cmake
+++ cmake/Modules/Testing.cmake
@@ -46,12 +46,12 @@ endfunction()
 if(BUILD_TESTING)
     find_package(Git REQUIRED)
 
-    execute_process(COMMAND ${GIT_EXECUTABLE} branch --contains
+    execute_process(COMMAND ${GIT_EXECUTABLE} branch --color=never --contains
         OUTPUT_VARIABLE CMAKE_SOURCE_BRANCH
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string(REPLACE "* " "" CMAKE_SOURCE_BRANCH "${CMAKE_SOURCE_BRANCH}")
     string(REPLACE "*" "" CMAKE_SOURCE_BRANCH "${CMAKE_SOURCE_BRANCH}")
-    string(REPLACE " " "" CMAKE_SOURCE_BRANCH "${CMAKE_SOURCE_BRANCH}")
 endif(BUILD_TESTING)
 
 
