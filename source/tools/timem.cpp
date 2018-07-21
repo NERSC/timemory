27367f83443d244cc3e7ad9886ecd12f3b3b3f50
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-07-21 01:55:44 -0700

    Improvements to "timem"

--- source/tools/timem.cpp
+++ source/tools/timem.cpp
@@ -24,20 +24,46 @@
 #include <chrono>
 #include <thread>
 
+#include <cstdio>
+#include <cstring>
+
 typedef std::vector<uint64_t> vector_t;
+typedef tim::rss::usage rss_usage_t;
 
 //----------------------------------------------------------------------------//
 
-tim::rss::usage rss_init;
-std::string tim_format =
-        ": %w wall, %u user + %s system = %t cpu (%p%) [%T], %M peak rss [%A]";
-std::string command = "";
+rss_usage_t& rss_init()
+{
+    static std::shared_ptr<rss_usage_t> _instance(nullptr);
+    if(!_instance.get())
+    {
+        _instance.reset(new rss_usage_t);
+    }
+    return *(_instance.get());
+}
+
+//----------------------------------------------------------------------------//
+
+std::string& tim_format()
+{
+    static std::string _instance
+            = ": %w wall, %u user + %s system = %t cpu (%p%) [%T], %M peak rss [%A]";
+    return _instance;
+}
+
+//----------------------------------------------------------------------------//
+
+std::string& command()
+{
+    static std::string _instance = "";
+    return _instance;
+}
 
 //----------------------------------------------------------------------------//
 
 void failed_fork()
 {
-    printf("can't fork, error occured\n");
+    printf("failure forking, error occured\n");
     exit(EXIT_FAILURE);
 }
 
@@ -46,13 +72,13 @@ void failed_fork()
 void report()
 {
     tim::manager::instance()->stop_total_timer();
-    (*tim::manager::instance()) -= rss_init;
-
     std::stringstream _ss_report;
+
+    (*tim::manager::instance()) -= rss_init();
     _ss_report << (*tim::manager::instance());
     std::string _report = _ss_report.str();
-    if(command.length() > 0)
-        _report.replace(_report.find("[exe]")+1, 3, command.c_str());
+    if(command().length() > 0)
+        _report.replace(_report.find("[exe]")+1, 3, command().c_str());
 
     std::cout << "\n" << _report << std::endl;
 
@@ -63,8 +89,6 @@ void report()
 
 void parent_process(pid_t pid)
 {
-    int status;
-
     // a positive number is returned for the pid of parent process
     // getppid() returns process id of parent of calling process
 
@@ -75,36 +99,54 @@ void parent_process(pid_t pid)
     // see wait() man page for all the flags or options
     // used here
 
+    int status;
+    int ret = 0;
+
     if (waitpid(pid, &status, 0) > 0)
     {
         if (WIFEXITED(status) && !WEXITSTATUS(status))
         {
-            if(getpid() != getppid() + 1)
-                report();
+            ret = 0;
         }
-        else if (WIFEXITED(status) && WEXITSTATUS(status))
+        else if(WIFEXITED(status) && WEXITSTATUS(status))
         {
-            #if defined(DEBUG)
-            if (WEXITSTATUS(status) == 127)
+            ret = WEXITSTATUS(status);
+            if(ret == 127)
                 printf("execv failed\n");
             else
-                printf("program terminated normally,"
-                       " but returned a non-zero status\n");
-            #endif
-            exit(WEXITSTATUS(status));
+                printf("program terminated with a non-zero status\n");
         }
         else
         {
-            #if defined(DEBUG)
-            printf("program didn't terminate normally\n");
-            #endif
-            exit(-1);
+            printf("program terminated annormally\n");
+            ret = -1;
         }
     }
     else
+    {
         printf("waitpid() failed\n");
+    }
 
-    exit(0);
+    if(getpid() != getppid() + 1)
+        report();
+
+    exit(ret);
+}
+
+//----------------------------------------------------------------------------//
+
+void print_command(int argc, char** argv)
+{
+    for(int i = 0; i < argc; ++i)
+        printf("%s ", argv[i]);
+    printf("\n");
+}
+
+//----------------------------------------------------------------------------//
+
+char* getcharptr(const std::string& str)
+{
+    return const_cast<char*>(str.c_str());
 }
 
 //----------------------------------------------------------------------------//
@@ -124,7 +166,22 @@ void child_process(int argc, char** argv)
     argv_list[argc-1] = NULL;
 
     // launch the child
-    execvp(argv_list[0], argv_list);
+    int ret = execvp(argv_list[0], argv_list);
+    if(ret < 0)
+    {
+        int argc_shell = argc + 2;
+        char** argv_shell_list = (char**) malloc(sizeof(char*) * argc_shell);
+        char* _shell = getusershell();
+        if(_shell)
+        {
+            argv_shell_list[0] = _shell;
+            argv_shell_list[1] = getcharptr("-lc");
+            for(int i = 0; i < argc-1; ++i)
+                argv_shell_list[i+2] = argv_list[i];
+            argv_shell_list[argc_shell-1] = NULL;
+            ret = execvp(argv_shell_list[0], argv_shell_list);
+        }
+    }
 
     exit(0);
 }
@@ -133,12 +190,12 @@ void child_process(int argc, char** argv)
 
 int main(int argc, char** argv)
 {
-    rss_init.record();
-    tim::format::timer::set_default_format(tim_format);
+    tim::format::timer::set_default_format(tim_format());
     tim::manager::instance()->update_total_timer_format();
+    rss_init().record();
 
     if(argc > 1)
-        command = std::string(const_cast<const char*>(argv[1]));
+        command() = std::string(const_cast<const char*>(argv[1]));
     else
     {
         tim::manager::instance()->reset_total_timer();
