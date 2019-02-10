ed9d7ced197251a8da7ed5eda00e213443132ba8
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-02-10 05:51:27 -0800

    Update base_clock.hpp
    - Fix for Windows

--- source/timemory/base_clock.hpp
+++ source/timemory/base_clock.hpp
@@ -106,6 +106,81 @@ EXTERN_C clock_t
 
 typedef long long suseconds_t;
 
+enum clockid_t
+{
+    CLOCK_REALTIME,
+    CLOCK_MONOTONIC,
+    CLOCK_MONOTONIC_RAW,
+    CLOCK_THREAD_CPUTIME_ID,
+    CLOCK_PROCESS_CPUTIME_ID
+};
+
+EXTERN_C LARGE_INTEGER
+         get_filetime_offset()
+{
+    SYSTEMTIME    s;
+    FILETIME      f;
+    LARGE_INTEGER t;
+
+    s.wYear         = 1970;
+    s.wMonth        = 1;
+    s.wDay          = 1;
+    s.wHour         = 0;
+    s.wMinute       = 0;
+    s.wSecond       = 0;
+    s.wMilliseconds = 0;
+    SystemTimeToFileTime(&s, &f);
+    t.QuadPart = f.dwHighDateTime;
+    t.QuadPart <<= 32;
+    t.QuadPart |= f.dwLowDateTime;
+    return (t);
+}
+
+EXTERN_C int
+clock_gettime(clockid_t, struct timeval* tv)
+{
+    LARGE_INTEGER        t;
+    FILETIME             f;
+    double               microseconds;
+    static LARGE_INTEGER offset;
+    static double        frequencyToMicroseconds;
+    static int           initialized           = 0;
+    static BOOL          usePerformanceCounter = 0;
+
+    if(!initialized)
+    {
+        LARGE_INTEGER performanceFrequency;
+        initialized           = 1;
+        usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
+        if(usePerformanceCounter)
+        {
+            QueryPerformanceCounter(&offset);
+            frequencyToMicroseconds = (double) performanceFrequency.QuadPart / 1000000.;
+        }
+        else
+        {
+            offset                  = get_filetime_offset();
+            frequencyToMicroseconds = 10.;
+        }
+    }
+    if(usePerformanceCounter)
+        QueryPerformanceCounter(&t);
+    else
+    {
+        GetSystemTimeAsFileTime(&f);
+        t.QuadPart = f.dwHighDateTime;
+        t.QuadPart <<= 32;
+        t.QuadPart |= f.dwLowDateTime;
+    }
+
+    t.QuadPart -= offset.QuadPart;
+    microseconds = (double) t.QuadPart / frequencyToMicroseconds;
+    t.QuadPart   = microseconds;
+    tv->tv_sec   = t.QuadPart / 1000000;
+    tv->tv_usec  = t.QuadPart % 1000000;
+    return (0);
+}
+
 #endif
 
 namespace tim
