f2b49975c729cb16f1152ff0d085753986fe3c09
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-12-19 21:42:26 -0800

    Minor update to python example

--- examples/ex-minimal/ex_python_minimal.py
+++ examples/ex-minimal/ex_python_minimal.py
@@ -2,7 +2,7 @@
 
 import sys
 from timemory.util import auto_timer
-
+import timemory
 
 def fib(n):
     return n if n < 2 else (fib(n-1) + fib(n-2))
@@ -20,3 +20,5 @@ def main(nfib):
 
 if __name__ == "__main__":
     main(int(sys.argv[1]) if len(sys.argv) > 1 else 34)
+    timemory.timemory_finalize()
+    
