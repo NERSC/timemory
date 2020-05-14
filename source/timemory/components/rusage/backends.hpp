d788a2b2009eb08384b1f13e8882aad4627c0c83
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2020-05-14 12:45:31 -0700

    Update RUSAGE_THREAD

--- source/timemory/components/rusage/backends.hpp
+++ source/timemory/components/rusage/backends.hpp
@@ -66,7 +66,7 @@ namespace tim
 {
 #if defined(_UNIX)
 
-#    if !defined(_GNU_SOURCE)
+#    if !defined(_GNU_SOURCE) || !defined(RUSAGE_THREAD)
 #        define RUSAGE_THREAD RUSAGE_SELF
 #    endif
 
