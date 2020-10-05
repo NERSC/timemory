ac4c72f29c7d044e76bc59b5e69c52e2398cb299
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2020-10-05 15:31:35 -0700

    Update pyctest-runner.py

--- pyctest-runner.py
+++ pyctest-runner.py
@@ -358,7 +358,7 @@ def run_pyctest():
     compiler_version = cmd.Output()
     try:
         cn = compiler_version.split()[0]
-        cv = re.search(r"(\b)\d.\d.\d", compiler_version)
+        cv = re.search(r"(\b)\d.\d.\d$", compiler_version)
         compiler_version = "{}-{}".format(cn, cv.group()[0])
     except Exception as e:
         print("Exception! {}".format(e))
