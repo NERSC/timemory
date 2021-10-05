a28de5c12f51d0bdebe136a3d01dc421aa4308eb
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2021-10-05 18:42:29 -0500

    Dyninst / timemory-run updates
    - added dyninst submodule
    - updated hatchet submodule
    - updated caliper submodule
    - fixed minor issue with configure_mpip
    - support for TIMEMORY_USE_MPI_HEADERS

--- source/timemory/components/gotcha/mpip.hpp
+++ source/timemory/components/gotcha/mpip.hpp
@@ -39,7 +39,7 @@
 #include <string>
 #include <unordered_map>
 
-#if defined(TIMEMORY_USE_MPI)
+#if defined(TIMEMORY_USE_MPI) || defined(TIMEMORY_USE_MPI_HEADERS)
 #    include <mpi.h>
 #endif
 
@@ -215,10 +215,11 @@ tim::component::deactivate_mpip(uint64_t id)
 //
 //======================================================================================//
 //
-#if !defined(TIMEMORY_USE_GOTCHA) || !defined(TIMEMORY_USE_MPI)
+#if !defined(TIMEMORY_USE_GOTCHA) ||                                                     \
+    (!defined(TIMEMORY_USE_MPI) && !defined(TIMEMORY_USE_MPI_HEADERS))
 //
 template <typename Toolset, typename Tag>
-void configure_mpip(std::set<std::string>, std::set<std::string>)
+void tim::component::configure_mpip(std::set<std::string>, std::set<std::string>)
 {}
 //
 #else
