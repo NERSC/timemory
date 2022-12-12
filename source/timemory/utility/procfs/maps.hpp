7e516ea5f003869cc93ddc9de9ab8f9ef98285cc
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2022-12-12 02:10:55 -0600

    procfs::maps updates
    - miscellaneous member functions: length, is_empty, is_file_mapping, permission query, is_contiguous_with, contains, operator+=, operator<
    - get_maps with filter functor
    - get_contiguous_maps
    - get_contiguous_maps with filter functor

--- source/timemory/utility/procfs/maps.hpp
+++ source/timemory/utility/procfs/maps.hpp
@@ -29,7 +29,9 @@
 #include "timemory/mpl/apply.hpp"
 #include "timemory/tpls/cereal/cereal/cereal.hpp"
 #include "timemory/utility/delimit.hpp"
+#include "timemory/utility/filepath.hpp"
 #include "timemory/utility/macros.hpp"
+#include "timemory/utility/transient_function.hpp"
 #include "timemory/variadic/macros.hpp"
 
 #include <array>
@@ -57,26 +59,26 @@ struct maps
     size_t              inode         = {};
     std::string         pathname      = {};
 
-    bool is_empty() const
-    {
-        // sum will be zero if default initialized
-        return ((start_address + end_address + offset + inode + device.length() +
-                 pathname.length()) == 0);
-    }
-
     template <typename ArchiveT>
-    void serialize(ArchiveT& ar, const unsigned)
-    {
-        auto _perms = std::string(permissions.size(), '-');
-        for(size_t i = 0; i < permissions.size(); ++i)
-            _perms.at(i) = permissions.at(i);
+    void serialize(ArchiveT&, const unsigned);
 
-        ar(cereal::make_nvp("start_address", TIMEMORY_JOIN("", std::hex, start_address)),
-           cereal::make_nvp("end_address", TIMEMORY_JOIN("", std::hex, end_address)),
-           cereal::make_nvp("permissions", _perms),
-           cereal::make_nvp("offset", TIMEMORY_JOIN("", std::hex, offset)),
-           cereal::make_nvp("device", device), cereal::make_nvp("inode", inode),
-           cereal::make_nvp("pathname", pathname));
+    size_t length() const;
+    bool   is_empty() const;
+    bool   is_file_mapping() const;
+    bool   has_read_perms() const { return permissions.at(0) == 'r'; }
+    bool   has_write_perms() const { return permissions.at(1) == 'w'; }
+    bool   has_exec_perms() const { return permissions.at(2) == 'x'; }
+    bool   is_private() const { return permissions.at(3) == 'p'; }
+    bool   is_contiguous_with(const maps&) const;
+    bool   contains(size_t) const;
+
+    maps& operator+=(const maps& rhs);
+
+    friend bool operator<(const maps& _lhs, const maps& _rhs)
+    {
+        return std::tie(_lhs.start_address, _lhs.end_address, _lhs.offset, _lhs.inode,
+                        _lhs.pathname) < std::tie(_rhs.start_address, _rhs.end_address,
+                                                  _rhs.offset, _rhs.inode, _rhs.pathname);
     }
 };
 
@@ -94,6 +96,77 @@ inline maps::maps(const std::vector<std::string>& _delim)
     inode  = std::stoull(_delim.at(4));
     if(_delim.size() > 5)
         pathname = _delim.at(5);
+}
+
+inline size_t
+maps::length() const
+{
+    return (end_address >= start_address) ? (end_address - start_address) : 0;
+}
+
+inline bool
+maps::is_empty() const
+{
+    // sum will be zero if default initialized
+    return ((start_address + end_address + offset + inode + device.length() +
+             pathname.length()) == 0);
+}
+
+template <typename ArchiveT>
+inline void
+maps::serialize(ArchiveT& ar, const unsigned)
+{
+    auto _perms = std::string(permissions.size(), '-');
+    for(size_t i = 0; i < permissions.size(); ++i)
+        _perms.at(i) = permissions.at(i);
+
+    ar(cereal::make_nvp("start_address", TIMEMORY_JOIN("", std::hex, start_address)),
+       cereal::make_nvp("end_address", TIMEMORY_JOIN("", std::hex, end_address)),
+       cereal::make_nvp("permissions", _perms),
+       cereal::make_nvp("offset", TIMEMORY_JOIN("", std::hex, offset)),
+       cereal::make_nvp("device", device), cereal::make_nvp("inode", inode),
+       cereal::make_nvp("pathname", pathname));
+}
+
+inline bool
+maps::is_file_mapping() const
+{
+    return !pathname.empty() && filepath::exists(pathname);
+}
+
+inline bool
+maps::contains(size_t _addr) const
+{
+    return _addr >= start_address && _addr < end_address;
+}
+
+inline bool
+maps::is_contiguous_with(const maps& _v) const
+{
+    return ((end_address == _v.start_address || start_address == _v.end_address) &&
+            pathname == _v.pathname);
+}
+
+inline maps&
+maps::operator+=(const maps& _v)
+{
+    if(is_contiguous_with(_v))
+    {
+        start_address = std::min<size_t>(start_address, _v.start_address);
+        end_address   = std::max<size_t>(end_address, _v.end_address);
+        offset        = std::min<size_t>(offset, _v.offset);
+        for(size_t i = 0; i < permissions.size(); ++i)
+        {
+            // downgrade permissions if not equal
+            if(permissions.at(i) != _v.permissions.at(i))
+                permissions.at(i) = (i == 4) ? 'p' : '-';
+        }
+        if(device != _v.device)
+            device += std::string{ ";" } + _v.device;
+        if(inode != _v.inode)
+            inode = 0;
+    }
+    return *this;
 }
 
 inline auto
@@ -140,6 +213,51 @@ get_maps(pid_t _pid, bool _update = false)
         _data.at(_pid) = read_maps(_pid);
 
     return _data.at(_pid);
+}
+
+inline auto
+get_maps(pid_t _pid, utility::transient_function<bool(const maps&)>&& _filter,
+         bool _update = false)
+{
+    auto _orig = get_maps(_pid, _update);
+    auto _data = std::vector<maps>{};
+    _data.reserve(_orig.size());
+    for(auto&& itr : _orig)
+        if(_filter(itr))
+            _data.emplace_back(itr);
+    return _data;
+}
+
+inline auto
+get_contiguous_maps(pid_t _pid, utility::transient_function<bool(const maps&)>&& _filter,
+                    bool _update = false)
+{
+    auto&& _find_contiguous = [](const auto& _v, auto _beg, auto _end) {
+        for(auto iitr = _beg; iitr != _end; ++iitr)
+            if(_v.pathname == iitr->pathname && _v.is_contiguous_with(*iitr))
+                return iitr;
+        return _end;
+    };
+
+    auto _data = std::vector<procfs::maps>{};
+    auto _orig = get_maps(_pid, std::move(_filter), _update);
+    for(const auto& itr : _orig)
+    {
+        auto iitr = _find_contiguous(itr, _data.begin(), _data.end());
+        if(iitr == _data.end())
+            _data.emplace_back(itr);
+        else
+            *iitr += itr;
+    }
+
+    return _data;
+}
+
+inline auto
+get_contiguous_maps(pid_t _pid, bool _update = false)
+{
+    return get_contiguous_maps(
+        _pid, [](const maps&) { return true; }, _update);
 }
 
 inline auto
