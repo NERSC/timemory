500ea2c2c6af83955f7ea02267418d0c9de89ab1
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2024-07-03 16:08:40 -0400

    Added alternative URL for binutils.

--- cmake/Modules/ConfigBinutils.cmake
+++ cmake/Modules/ConfigBinutils.cmake
@@ -75,6 +75,7 @@ externalproject_add(
     binutils-external
     PREFIX ${PROJECT_BINARY_DIR}/external/binutils
     URL http://ftpmirror.gnu.org/gnu/binutils/binutils-2.40.tar.gz
+        http://mirrors.kernel.org/sourceware/binutils/releases/binutils-2.40.tar.gz
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND
         ${CMAKE_COMMAND} -E env CC=${CMAKE_C_COMPILER} CFLAGS=-fPIC\ -O3
